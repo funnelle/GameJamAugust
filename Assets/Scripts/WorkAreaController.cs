acb8382bd2f4c22066b512509584f394faf02857
Evan Funnell <funnelle@mymacewan.ca>
2018-08-17 23:14:45 -0600

    Resized other UI elements

---
+++ Assets/Scripts/WorkAreaController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkAreaController : MonoBehaviour {
+    public int DataCollectedCount;
+    public Text DataCollectedText;
+
+    private void Start() {
+        DataCollectedCount = 0;
+        SetCountText();
+    }
+
+    public void IncreaseDataCollected() {
+        DataCollectedCount++;
+        SetCountText();
+    }
+
+    void SetCountText() {
+        DataCollectedText.text = "Data Collected: " + DataCollectedCount.ToString();
+    }
+}
