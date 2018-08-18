36c6e37501dce623b207216608623d3e155fb3c4
EvanGee <evangiese77@gmail.com>
2018-08-18 15:28:02 -0600

    Avatar

---
+++ Assets/Scripts/Avatar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Avatar : MonoBehaviour {
+    
+    bool expanded;
+    public Button avatarButton; 
+    public Canvas bubble;
+
+	// Use this for initialization
+	void Start () {
+        
+        avatarButton.onClick.AddListener(OnClick);
+        expanded = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void OnClick()
+    {
+        if (expanded)
+            collapse();
+        else
+            expand();
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+    }
+
+    void expand()
+    {
+        expanded = true;
+        bubble.enabled = true;
+    }
+    void collapse()
+    {
+        expanded = false;
+        bubble.enabled = false;
+
+    }
+
+}
