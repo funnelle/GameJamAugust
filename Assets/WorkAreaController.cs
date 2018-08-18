43f945b8a34c502fb95b89fc0f79bde7a2c9b48f
Evan Funnell <funnelle@mymacewan.ca>
2018-08-17 22:26:12 -0600

    Data Mine button increases count

---
+++ Assets/WorkAreaController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkAreaController : MonoBehaviour {
+    public int DataCollectedCount = 0;
+
+    public void IncreaseDataCollected() {
+        DataCollectedCount++;
+    }
+}
