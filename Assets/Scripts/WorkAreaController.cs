a7cdae491305bbf6247354f3dc4e2d4cf79a8ed8
Evan Funnell <funnelle@mymacewan.ca>
2018-08-18 01:39:29 -0600

    3 buttons function, basic gameplay loop functional

--- Assets/Scripts/WorkAreaController.cs
+++ Assets/Scripts/WorkAreaController.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class WorkAreaController : MonoBehaviour {
-    public int DataCollectedCount;
-    public Text DataCollectedText;
+    public int DataSetsCount;
+    public int NewFollowersCount;
+    public int RevenueCount;
+    public int UsersCount;
+    public int PentaPointsCount;
+
+    public Text DataSetsText;
+    public Text NewFollowersText;
+    public Text RevenueText;
+    public Text UsersText;
+    public Text PentaPointsText;
 
     private void Start() {
-        DataCollectedCount = 0;
-        SetCountText();
-    }
-
-    public void IncreaseDataCollected() {
-        DataCollectedCount++;
+        DataSetsCount = 0;
+        NewFollowersCount = 0;
+        RevenueCount = 500;
+        UsersCount = 0;
+        PentaPointsCount = 0;
         SetCountText();
     }
 
     void SetCountText() {
-        DataCollectedText.text = "Data Collected: " + DataCollectedCount.ToString();
+        DataSetsText.text = "Data Sets: " + DataSetsCount.ToString();
+        NewFollowersText.text = "New Followers: " + NewFollowersCount.ToString();
+        RevenueText.text = "Revenue: $" + RevenueCount.ToString();
+        UsersText.text = "Users: " + UsersCount.ToString();
+        PentaPointsText.text = "Penta-Points: " + PentaPointsCount.ToString();
+    }
+
+    public void DataMine() {
+        if (NewFollowersCount >= 1) {
+            DataSetsCount++;
+            NewFollowersCount--;
+            UsersCount++;
+            SetCountText();
+        }
+    }
+
+    public void DeployAd() {
+        if (RevenueCount >= 500) {
+            RevenueCount = RevenueCount - 500;
+            NewFollowersCount++;
+            SetCountText();
+        }
+    }
+
+    public void SellData() {
+        if (DataSetsCount>=1) {
+            DataSetsCount--;
+            RevenueCount = RevenueCount + 500;
+            SetCountText();
+        }
     }
 }
