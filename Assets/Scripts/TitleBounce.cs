75c0de44d56c7c78313652790229dd80895d3eda
Evan Funnell <funnelle@mymacewan.ca>
2018-08-19 12:33:52 -0600

    Fixed music not looping, added bouncing title

---
+++ Assets/Scripts/TitleBounce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleBounce : MonoBehaviour {
+    public Image titleImage;
+
+    public float speed;
+    public float maxRotation;
+    public float maxZoom;
+	
+	// Update is called once per frame
+	void Update () {
+        titleImage.rectTransform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+        titleImage.rectTransform.localScale = new Vector3(maxZoom * Mathf.Sin(Time.time * speed), maxZoom * Mathf.Sin(Time.time * speed), 0f);
+	}
+}
